ee439b152c7d558e959542646cf7508aaf90b7fa
Darrien Singleton <onethatwalks@aol.com>
2012-01-12 16:53:15 -0500

    Project set-up for reals

---
+++ src/monopoly/property/BalticAvenue.java
@@ -0,0 +1,16 @@
+package monopoly.property;
+
+import java.awt.*;
+
+public class BalticAvenue extends Property {
+
+	public BalticAvenue() {
+		rent[0] = 4;
+		set = 1;
+		cost = 60;
+		for (int i = 0; i < 4; i++) {
+			person[i] = new Dimension(2 * i, 2 * i);
+			building[i] = new Dimension((2 * i) + 1, (2 * i) + 1);
+		}
+	}
+}
---
+++ src/monopoly/property/MediterraneanAvenue.java
@@ -0,0 +1,17 @@
+package monopoly.property;
+
+import java.awt.*;
+
+public class MediterraneanAvenue extends Property {
+
+	public MediterraneanAvenue() {
+		rent[0] = 2;
+		set = 1;
+		cost = 60;
+		for (int i = 0; i < 4; i++) {
+			person[i] = new Dimension(2 * i, 2 * i);
+			building[i] = new Dimension((2 * i) + 1, (2 * i) + 1);
+		}
+	}
+
+}
